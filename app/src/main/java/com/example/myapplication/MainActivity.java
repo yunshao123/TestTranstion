70e014af75d4c634952934290bc82a1680d24c64
49545 <495454126@qq.com>
2024-05-09 00:01:42 +0800

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,22 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         setContentView(R.layout.activity_main);
         TextView tvShow = findViewById(R.id.tvShow);
+        Button button = findViewById(R.id.btn1);
+        VideoView videoView = findViewById(R.id.video1);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+        videoView.start();
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                videoView.start();
+            }
+        });
         tvShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,TestActivity.class);
-                ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this);
+                ActivityOptions activityOptions = ActivityOptions
+                        .makeSceneTransitionAnimation(MainActivity.this, Pair.create(button,"button"),
+                                Pair.create(videoView,"videoView"));
                 startActivity(intent,activityOptions.toBundle());
             }
         });
--- app/src/main/java/com/example/myapplication/TestActivity.java
+++ app/src/main/java/com/example/myapplication/TestActivity.java
@@ -2,12 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.transition.Explode;
 import android.transition.Fade;
 import android.transition.Slide;
+import android.transition.Transition;
 import android.transition.TransitionSet;
+import android.util.Log;
 import android.view.Gravity;
+import android.widget.VideoView;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -32,5 +36,48 @@ public class TestActivity extends AppCompatActivity {
         //进入
         getWindow().setEnterTransition(transitionSet);
         setContentView(R.layout.activity_test);
+        VideoView videoView = findViewById(R.id.video2);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+        videoView.start();
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                videoView.start();
+            }
+        });
+        // 启动动画过渡
+        supportStartPostponedEnterTransition();
+        Transition transition = getWindow().getSharedElementEnterTransition();
+        transition.setDuration(3000);
+        Log.i("测试=====","000000000000000");
+        if (transition != null) {
+            Log.i("测试=====","11111111111111111");
+            transition.addListener(new Transition.TransitionListener() {
+                @Override
+                public void onTransitionStart(Transition transition) {
+                    Log.i("测试=====","222222222222");
+                }
+
+                @Override
+                public void onTransitionEnd(Transition transition) {
+                    Log.i("测试=====","3333333333333333");
+                }
+
+                @Override
+                public void onTransitionCancel(Transition transition) {
+
+                }
+
+                @Override
+                public void onTransitionPause(Transition transition) {
+
+                }
+
+                @Override
+                public void onTransitionResume(Transition transition) {
+
+                }
+            });
+        }
     }
 }
