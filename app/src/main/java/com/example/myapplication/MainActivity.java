5d731055811972c2c4e8fa0a0af35c646eb56b55
49545 <495454126@qq.com>
2024-06-03 00:18:18 +0800

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import static android.transition.Visibility.MODE_IN;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
@@ -10,9 +12,14 @@ import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.transition.ArcMotion;
+import android.transition.ChangeBounds;
+import android.transition.ChangeTransform;
+import android.transition.Fade;
 import android.transition.Slide;
 import android.transition.Transition;
 import android.transition.TransitionSet;
+import android.transition.Visibility;
 import android.util.Log;
 import android.util.Pair;
 import android.view.Gravity;
@@ -72,8 +79,49 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,TestActivity.class);
                 TransitionSet transition = new TransitionSet();
-                transition.addTransition(new Slide(Gravity.LEFT).addTarget(R.id.btn1));
+//                transition.addTransition(new Slide(Gravity.LEFT).addTarget(R.id.btn1));
+                ChangeTransform changeTransform = new ChangeTransform();
+                Button button1 = findViewById(R.id.btn1);
+                changeTransform.addTarget(button1);
+                button1.setScaleX(2f);
+                button1.setScaleY(2f);
+                transition.addTransition(changeTransform);
+
+                Fade fade = new Fade();
+                fade.setMode(Visibility.MODE_OUT);
+                fade.addTarget(button1);
+
+                transition.setDuration(2000L);
+                transition.addTransition(fade);
+
+
                 getWindow().setExitTransition(transition);
+                transition.addListener(new Transition.TransitionListener() {
+                    @Override
+                    public void onTransitionStart(Transition transition) {
+
+                    }
+
+                    @Override
+                    public void onTransitionEnd(Transition transition) {
+                        Log.i("测试=====","+++++++++++++++++++++");
+                    }
+
+                    @Override
+                    public void onTransitionCancel(Transition transition) {
+
+                    }
+
+                    @Override
+                    public void onTransitionPause(Transition transition) {
+
+                    }
+
+                    @Override
+                    public void onTransitionResume(Transition transition) {
+
+                    }
+                });
                 ActivityOptions activityOptions = ActivityOptions
                         .makeSceneTransitionAnimation(MainActivity.this);
                 startActivity(intent,activityOptions.toBundle());
