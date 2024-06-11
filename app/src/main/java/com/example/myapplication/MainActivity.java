38beaba6d372ce5f8fd75220b7c8b05e878a301c
49545 <495454126@qq.com>
2024-06-12 00:16:31 +0800

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,32 +1,49 @@
 package com.example.myapplication;
 
+import static android.transition.TransitionSet.ORDERING_TOGETHER;
 import static android.transition.Visibility.MODE_IN;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.ImageSpan;
 import android.transition.ArcMotion;
 import android.transition.ChangeBounds;
 import android.transition.ChangeTransform;
 import android.transition.Fade;
 import android.transition.Slide;
 import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.transition.TransitionSet;
 import android.transition.Visibility;
 import android.util.Log;
 import android.util.Pair;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -37,36 +54,17 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
     private int progress;
 
-    private VideoView videoView;
-
     private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
         setContentView(R.layout.activity_main);
         TextView tvShow = findViewById(R.id.tvShow);
         Button button = findViewById(R.id.btn1);
-        videoView = findViewById(R.id.video1);
         imageView = findViewById(R.id.imageview);
-        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
-        String path = "android.resource://" + getPackageName() + "/" + R.raw.video;
-//        imageView.setImageResource(R.drawable.ic_launcher_background);
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.setOnInfoListener(new MediaPlayer.OnInfoListener() {
-                    @Override
-                    public boolean onInfo(MediaPlayer mediaPlayer, int i, int i1) {
-                        if (i == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START){
-                            imageView.setVisibility(View.GONE);
-                        }
-                        return true;
-                    }
-                });
-            }
-        });
+        imageView.setImageResource(R.drawable.ic_launcher_background);
 //        videoView.start();
 //        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 //            @Override
@@ -74,57 +72,22 @@ public class MainActivity extends AppCompatActivity {
 //                videoView.start();
 //            }
 //        });
+        button.setVisibility(View.VISIBLE);
         tvShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,TestActivity.class);
-                TransitionSet transition = new TransitionSet();
-//                transition.addTransition(new Slide(Gravity.LEFT).addTarget(R.id.btn1));
-                ChangeTransform changeTransform = new ChangeTransform();
-                Button button1 = findViewById(R.id.btn1);
-                changeTransform.addTarget(button1);
-                button1.setScaleX(2f);
-                button1.setScaleY(2f);
-                transition.addTransition(changeTransform);
+                ObjectAnimator
+                AnimatorSet animatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(MainActivity.this, R.animator.test_animator);
+                animatorSet.setTarget();
+                animatorSet.start();
 
-                Fade fade = new Fade();
-                fade.setMode(Visibility.MODE_OUT);
-                fade.addTarget(button1);
-
-                transition.setDuration(2000L);
-                transition.addTransition(fade);
-
-
-                getWindow().setExitTransition(transition);
-                transition.addListener(new Transition.TransitionListener() {
-                    @Override
-                    public void onTransitionStart(Transition transition) {
-
-                    }
-
-                    @Override
-                    public void onTransitionEnd(Transition transition) {
-                        Log.i("测试=====","+++++++++++++++++++++");
-                    }
-
-                    @Override
-                    public void onTransitionCancel(Transition transition) {
-
-                    }
-
-                    @Override
-                    public void onTransitionPause(Transition transition) {
-
-                    }
-
-                    @Override
-                    public void onTransitionResume(Transition transition) {
-
-                    }
-                });
-                ActivityOptions activityOptions = ActivityOptions
-                        .makeSceneTransitionAnimation(MainActivity.this);
-                startActivity(intent,activityOptions.toBundle());
+//                getWindow().setExitTransition(transition);
+//                Log.i("测试=====","......................");
+//                ActivityOptions activityOptions = ActivityOptions
+//                        .makeSceneTransitionAnimation(MainActivity.this,button,"button");
+//                startActivity(intent,activityOptions.toBundle());
+//                getWindow().getSharedElementExitTransition().setDuration(3000L);
             }
         });
     }
@@ -140,7 +103,63 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        progress = videoView.getCurrentPosition();
-        videoView.pause();
+    }
+
+    private void addTagToTextView(TextView target, String title, String tag) {
+        if (TextUtils.isEmpty(title)) {
+            title = "";
+        }
+
+        String content = title + tag;
+
+
+        /**
+         * 创建TextView对象，设置drawable背景，设置字体样式，设置间距，设置文本等
+         * 这里我们为了给TextView设置margin，给其添加了一个父容器LinearLayout。不过他俩都只是new出来的，不会添加进任何布局
+         */
+        LinearLayout layout = new LinearLayout(this);
+        TextView textView = new TextView(this);
+        textView.setText(tag);
+        textView.setBackground(getResources().getDrawable(R.drawable.room_member_role_bg));
+        textView.setTextSize(12);
+        textView.setTextColor(Color.parseColor("#FDA413"));
+        textView.setIncludeFontPadding(false);
+        textView.setPadding(ScreenUtils.dip2px(this, 6), 0,
+                ScreenUtils.dip2px(this, 6), 0);
+        textView.setHeight(ScreenUtils.dip2px(this, 17));
+        textView.setGravity(Gravity.CENTER_VERTICAL);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        // 设置左间距
+        layoutParams.leftMargin = ScreenUtils.dip2px(this, 6);
+        // 设置下间距，简单解决ImageSpan和文本竖直方向对齐的问题
+        layoutParams.bottomMargin = ScreenUtils.dip2px(this, 3);
+        layout.addView(textView, layoutParams);
+
+        /**
+         * 第二步，测量，绘制layout，生成对应的bitmap对象
+         */
+        layout.setDrawingCacheEnabled(true);
+        layout.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        // 给上方设置的margin留出空间
+        layout.layout(0, 0, textView.getMeasuredWidth() + ScreenUtils.dip2px(this, (6 + 3)), textView.getMeasuredHeight());
+        // 获取bitmap对象
+        Bitmap bitmap = Bitmap.createBitmap(layout.getDrawingCache());
+        //千万别忘最后一步
+        layout.destroyDrawingCache();
+
+        /**
+         * 第三步，通过bitmap生成我们需要的ImageSpan对象
+         */
+        ImageSpan imageSpan = new ImageSpan(this, bitmap);
+
+
+        /**
+         * 第四步将ImageSpan对象设置到SpannableStringBuilder的对应位置
+         */
+        SpannableStringBuilder ssb = new SpannableStringBuilder(content);
+        //将尾部tag字符用ImageSpan替换
+        ssb.setSpan(imageSpan, title.length(), content.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        target.setText(ssb);
     }
 }
--- app/src/main/java/com/example/myapplication/TestActivity.java
+++ app/src/main/java/com/example/myapplication/TestActivity.java
@@ -12,6 +12,7 @@ import android.transition.Transition;
 import android.transition.TransitionSet;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Window;
 import android.widget.VideoView;
 
 public class TestActivity extends AppCompatActivity {
@@ -19,37 +20,31 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TransitionSet transitionSet = new TransitionSet();
-        transitionSet.setDuration(300);
-        //一起动画
-        transitionSet.setOrdering(TransitionSet.ORDERING_TOGETHER);
-        Slide slideTransition = new Slide();
-        slideTransition.setSlideEdge(Gravity.RIGHT);
-        transitionSet.addTransition(slideTransition);
-        Fade fadeTransition = new Fade();
-        transitionSet.addTransition(fadeTransition);
-        //排除状态栏
-        transitionSet.excludeTarget(android.R.id.statusBarBackground, true);
-        transitionSet.excludeTarget(android.R.id.navigationBarBackground, true);
-        //是否同时执行
+        getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
         getWindow().setAllowEnterTransitionOverlap(false);
-        getWindow().setAllowReturnTransitionOverlap(false);
+//        TransitionSet transitionSet = new TransitionSet();
+//        transitionSet.setDuration(300);
+//        //一起动画
+//        transitionSet.setOrdering(TransitionSet.ORDERING_TOGETHER);
+//        Slide slideTransition = new Slide();
+//        slideTransition.setSlideEdge(Gravity.RIGHT);
+//        transitionSet.addTransition(slideTransition);
+//        Fade fadeTransition = new Fade();
+//        transitionSet.addTransition(fadeTransition);
+//        //排除状态栏
+//        transitionSet.excludeTarget(android.R.id.statusBarBackground, true);
+//        transitionSet.excludeTarget(android.R.id.navigationBarBackground, true);
+//        //是否同时执行
+//        getWindow().setAllowEnterTransitionOverlap(false);
+//        getWindow().setAllowReturnTransitionOverlap(false);
         //进入
-        getWindow().setEnterTransition(transitionSet);
+//        getWindow().setEnterTransition(transitionSet);
         setContentView(R.layout.activity_test);
-        VideoView videoView = findViewById(R.id.video2);
-        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
-        videoView.start();
-        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                videoView.start();
-            }
-        });
+
         // 启动动画过渡
-        supportStartPostponedEnterTransition();
+//        supportStartPostponedEnterTransition();
         Transition transition = getWindow().getSharedElementEnterTransition();
-        transition.setDuration(3000);
+        transition.setDuration(3000L);
         Log.i("测试=====","000000000000000");
         if (transition != null) {
             Log.i("测试=====","11111111111111111");
